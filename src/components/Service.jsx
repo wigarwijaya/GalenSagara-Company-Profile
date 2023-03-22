47ac0b0a88c7dfb386b203e71d9f6946c1044970
R. Wigar Wijaya W <wigarwijaya@gmail.com>
2023-03-23 00:28:55 +0700

    enhancing Services component

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -5,7 +5,7 @@ const Service = () => {
   return (
     <section className="text-gray-600 body-font">
       <div className="container mx-auto px-5 py-24">
-        <div className="flex flex-col text-center w-full mb-20">
+        <div className="flex flex-col text-center w-full mb-7">
           <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
             Services
           </h1>
